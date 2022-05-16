101b17ed52b200b5a253d8fd73634cad6bd66a87
Edvaldo Afonso <eadafonso@gmail.com>
2022-05-16 16:23:34 +0100

    feat: configure next-auth

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,39 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import prisma from "../../../lib/prisma";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          username: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+        username: user.username,
+      },
+    }),
+  },
+};
+
+export default NextAuth(authOptions);
