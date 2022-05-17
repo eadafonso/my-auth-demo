08ebfd7dfff0eb576f54c1b2a5262bf044d4bd72
Edvaldo Afonso <eadafonso@gmail.com>
2022-05-17 18:50:09 +0100

    feat: chore create next auth login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,39 @@
 import type { NextPage } from "next";
+import { getSession, signOut, useSession } from "next-auth/react";
+import useRequireAuth from "../lib/useRequireAuth";
 
 const Home: NextPage = () => {
-  return <h1>Home</h1>;
+  const { data: session } = useSession();
+
+  /*   const session = useRequireAuth();
+  if (!session) return <div>loading...</div>; */
+
+  return (
+    <div>
+      <h1>{`Seja bem-vindo ${session?.user?.name}`}</h1>
+      <button onClick={() => signOut()}>Sair</button>
+    </div>
+  );
 };
 
 export default Home;
+
+/* export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
+ */
